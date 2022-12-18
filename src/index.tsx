f0fd76193dacdaa0f3acc4279856aaa4e8fbd807
Akinyemi Temiye <akinyemitemiye18@gmail.com>
2022-12-18 15:26:28 +0100

    enable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <ThemeContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeContextProvider>
+  <React.StrictMode>
+    <ThemeContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeContextProvider>
+  </React.StrictMode>
 );
